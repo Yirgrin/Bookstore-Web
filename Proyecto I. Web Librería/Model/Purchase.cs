92376b4583f0ca0dac2348af0d7813fd033a9ff0
Yirgrin <ybermuddez@gmail.com>
2023-04-27 18:27:05 -0600

    actualización

---
+++ Proyecto I. Web Librería/Model/Purchase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bookstore_Web.Model
+{
+	public class Purchase
+	{
+        public string Client { get; set; }
+        public string Email { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public int PostalCode { get; set; }
+        public int CardNumber { get; set; }
+        public DateTime ExpDate { get; set; }
+        public int SecurityCode { get; set; }
+    }
+}
